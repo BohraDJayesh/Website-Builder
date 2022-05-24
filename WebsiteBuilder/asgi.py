0bc8542826bb6ffacc3a55e036530b8adc8610e7
Abhay <you@example.com>
2022-05-24 18:06:20 +0530

    Add Dj-ango files

---
+++ WebsiteBuilder/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for WebsiteBuilder project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebsiteBuilder.settings')
+
+application = get_asgi_application()
