b945c0c7fc55e6ec339335ebf89fa08d684a5ac1
abhay2912 <abhaygupta5432@gmail.com>
2022-06-03 11:46:05 +0530

    created base.html file and editable page(as index.html) in templates folder and added CSS & Script files in static folder

---
+++ static/JavaScript/script.js
@@ -0,0 +1,37 @@
+
+//------------------- script for the editor ---------------------------------------------------------------
+
+/*
+
+const editor = grapesjs.init({
+
+   
+      // Indicate where to init the editor. You can also pass an HTMLElement
+  container: '#gjs',
+  // Get the content for the canvas directly from the element
+  // As an alternative we could use: `components: '<h1>Hello World Component!</h1>'`,
+  fromElement: true,
+  // Size of the editor
+  // height: '300px',
+  // width: 'auto',
+  // Disable the storage manager for the moment
+  storageManager: false,
+  // Avoid any default panel
+  panels: { defaults: [] },
+  });
+  */
+  const editor = grapesjs.init({
+    // Indicate where to init the editor. You can also pass an HTMLElement
+    container: '#gjs',
+    // Get the content for the canvas directly from the element
+    // As an alternative we could use: `components: '<h1>Hello World Component!</h1>'`,
+    fromElement: true,
+    // Size of the editor
+    height: '300px',
+    width: 'auto',
+    // Disable the storage manager for the moment
+    storageManager: false,
+    // Avoid any default panel
+    panels: { defaults: [] },
+  });
+
